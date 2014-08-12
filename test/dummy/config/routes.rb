636d2404e0a59262fc2c87b4aada062630938ce9
Pritesh <pritesh@phatforge.com>
2014-08-12 09:31:32 +0100

    initial post show controller action for integration test

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
+  get "post/show"
+
   mount ThemeRenderer::Engine => "/theme_renderer"
 end
---
+++ test/integration/file_store_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+describe "Render File store templates" do
+
+  before { visit posts_show_path }
+  it 'render posts/show from file store' do
+  end
+end
